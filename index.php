f66814f986ca298dff836387bbfe8dc0021ce1ae
zinkerzumper2112 <andro.ug.meparishvili@gmail.com>
2021-10-08 22:16:05 +0400

    davaleba 3

--- index.php
+++ index.php
@@ -1,40 +1,30 @@
-<?php
-    function build_table($array){
-    // start table
-    $html = '<table>';
-    // header row
-    $html .= '<tr>';
-    foreach($array[0] as $key=>$value){
-            $html .= '<th>' . htmlspecialchars($key) . '</th>';
-        }
-    $html .= '</tr>';
+<?php 
+// index.php
+session_start();
 
-    // data rows
-    foreach( $array as $key=>$value){
-        $html .= '<tr>';
-        foreach($value as $key2=>$value2){
-            $html .= '<td>' . htmlspecialchars($value2) . '</td>';
-        }
-        $html .= '</tr>';
-    }
-
-    // finish table and return it
-
-    $html .= '</table>';
-    return $html;
+// If user is logged in, retrieve identity from session.
+$identity = null;
+if (isset($_SESSION['identity'])) {
+    $identity = $_SESSION['identity'];
 }
-
-$array = array(
-    array('id'=>'1', 'name'=>'Giorgi', 'lastname'=>'Amashukeli', 'country'=>'Georgia', 'skills'=>"html, css, js", 'available'=>"true"),
-    array('id'=>'2', 'name'=>'Mariam', 'lastname'=>'Abashidze', 'country'=>'Georgia', 'skills'=>"html", 'available'=>"true"),
-    array('id'=>'3', 'name'=>'Andria', 'lastname'=>'Beridze', 'country'=>'USA', 'skills'=>"usless", 'available'=>"false"),
-    array('id'=>'4', 'name'=>'Nino', 'lastname'=>'Nozadze', 'country'=>'Germany', 'skills'=>"Js, css", 'available'=>"false")
-
-);
-
-    echo build_table($array);
 ?>
 
-
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Home page</title>
+    </head>
+    <body>
+        <h1>Home</h1>
+        <?php if ($identity==null): ?>
+        <a href="login.php">Sign in</a>
+        <?php else: ?>
+        <strong>Welcome, <?= $identity ?></strong> <a href="logout.php">Sign out</a>
+        <?php endif; ?>
         
-    
+        <p>
+            This is a simple website to demonstrate the advantages of a PHP framework
+            and disadvantages of "pure" PHP.
+        </p>
+    </body>
+</html>
